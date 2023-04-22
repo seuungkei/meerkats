fe7ae9574875787a0a29b4e48932ef6d3a5e08cb
seuungkei <seuungkei@gmail.com>
2023-04-22 18:35:06 +0900

    [ADD] 영화 상세페이지 기능 구현

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,10 @@
 import * as express from 'express';
+import { userRouter } from './userRouter';
+import { movieRouter } from './movieRouter';
 
 const router = express.Router();
+
+router.use('/users', userRouter);
+router.use('/movie', movieRouter);
 
 export default router;
